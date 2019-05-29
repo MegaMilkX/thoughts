eec412b28b31d7abfe1e5a79104bc156ff2dea70
megamilkx <gyrocopter3k@gmail.com>
2019-05-30 01:13:13 +0300

    Init

---
+++ main.cpp
@@ -0,0 +1,27 @@
+
+class SceneSession;
+class SceneContext;
+
+class attrTransform;
+class attrAnimStack;
+class attrModel;
+class attrLightOmni;
+
+class bhvrCharacter;
+class bhvrTpsCamera;
+class bhvrCollisionTrigger;
+
+namespace game {
+
+void gameStart() {
+	respawnPlayer();
+	
+	showSplashText("Hello, World!");
+}
+
+
+void gameUpdate() {
+	
+}
+
+}
